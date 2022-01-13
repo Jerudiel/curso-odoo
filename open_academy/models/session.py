c5726ea99f928a98c7b0e24ccc9ec1a7ba7e60c3
Benito Jerudiel Ortiz Pérez <jerudiel@vauxoo.com>
2022-01-13 17:15:52 -0600

    [IMP] open_academy: Add onchange in session fields: number_of_seats and attendees, to verify there is a positive number of seats and these are more than or equal to number of attendees, if not, show a warning message

--- open_academy/models/session.py
+++ open_academy/models/session.py
@@ -1,4 +1,3 @@
-from email.policy import default
 from odoo import models, fields, api
 
 
@@ -7,7 +6,7 @@ class Session(models.Model):
     _description = 'Session'
 
     name = fields.Char()
-    start_date = fields.Datetime(default=lambda self: fields.Date.today())
+    start_date = fields.Datetime(default=lambda self: fields.Date.today()) 
     duration = fields.Float()
     number_of_seats = fields.Integer()
     instructor_id = fields.Many2one("res.partner", domain="['|',('is_instructor','=',True),('category_id.name','like','Teacher')]")
@@ -20,3 +19,14 @@ class Session(models.Model):
     def _compute_percentage_of_taken_seats(self):
         for record in self:
             record.percentage_of_taken_seats = int((len(record.attendee_ids)*100)/record.number_of_seats) if record.number_of_seats > 0 else 0
+    
+    @api.onchange('number_of_seats','attendee_ids')
+    def _onchange_number_of_seats_and_attendee_ids(self):
+        if self.number_of_seats < 0:
+            return {'warning': {
+            'title': "Problem in number of seats",
+            'message': "The number of seats must be positive",}}
+        elif len(self.attendee_ids) > self.number_of_seats:
+            return {'warning': {
+            'title': "Problem in number of attendees",
+            'message': "The number of attendees is more than number of seats available",}}
