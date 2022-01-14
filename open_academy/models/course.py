6b5969d378f88081219e16b57c5cef96b08fcb6e
Benito Jerudiel Ortiz Pérez <jerudiel@vauxoo.com>
2022-01-14 16:15:01 -0600

    [IMP] open_academy: Add SQL constraints, in the course model, title and description must be differents and title must be unique

--- open_academy/models/course.py
+++ open_academy/models/course.py
@@ -9,3 +9,8 @@ class Course(models.Model):
     description = fields.Text()
     responsible_user_id = fields.Many2one("res.users")
     session_ids = fields.One2many("session", "course_id")
+
+    _sql_constraints = [
+        ('description_and_title_are_differents', 'CHECK (title <> description)', 'Title and description must be differents'),
+        ('title_unique', 'UNIQUE (title)', 'Title must be unique'),
+    ]
