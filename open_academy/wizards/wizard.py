1e9d2fae835f1ad93037215046128820b97a80ad
Benito Jerudiel Ortiz Pérez <jerudiel@vauxoo.com>
2022-01-21 12:15:59 -0600

    [IMP] open_academy: wizard model now can register attendees in multiple sessions

--- open_academy/wizards/wizard.py
+++ open_academy/wizards/wizard.py
@@ -5,8 +5,10 @@ class AssignAttendeeSessions(models.TransientModel):
     _name = 'assing.attendee.sessions'
     _description = 'Assign Attendee Sessions'
 
-    session_id = fields.Many2one('session')
+    session_ids = fields.Many2many('session')
     attendee_ids = fields.Many2many('res.partner')
 
-    def add_attendees_session(self):
-        self.session_id.write({'attendee_ids': [Command.link(attendee.id) for attendee in self.attendee_ids]})
+    def action_register_attendees_to_sessions(self):
+        for session in self.session_ids:
+            if len(session.attendee_ids) + len(self.attendee_ids) <= session.number_of_seats:
+                session.write({'attendee_ids': [Command.link(attendee.id) for attendee in self.attendee_ids]})
